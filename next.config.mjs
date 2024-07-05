a3266269d2001570b0e61894388647a6cf5611bb
Karistophal <charlesdelfau@gmail.com>
2024-07-05 02:08:47 +0200

    defaultUserImage

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['lh3.googleusercontent.com', 'www.trace-ta-route.com', 'images.unsplash.com', 'www.bfmtv.com']
+        domains: ['lh3.googleusercontent.com', 'www.trace-ta-route.com', 'images.unsplash.com', 'www.bfmtv.com', "proflex-mu.vercel.app"]
     },
     
 };
